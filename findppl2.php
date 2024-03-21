c91c68a3bd4990fad0367346535d1ab6e83a8c1c
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2024-03-21 11:06:05 -0400

    Update findppl2.php

--- findppl2.php
+++ findppl2.php
@@ -41,7 +41,7 @@
     $servername = "localhost"; // Change if your MySQL server is hosted elsewhere
     $username = "root"; // Your MySQL username
     $password = ""; // Your MySQL password
-    $dbname = "cosi127_pa1_2"; // Your MySQL database name
+    $dbname = "cosi127_pa1_3"; // Your MySQL database name
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
