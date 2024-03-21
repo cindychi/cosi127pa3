b8f9ba673ea275e9c8f7b05fe7ece7fdeca34835
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2024-03-21 10:58:09 -0400

    Update indexFinal.php

--- indexFinal.php
+++ indexFinal.php
@@ -18,25 +18,12 @@
 <html>
 <head>
     <title>IMDB Movie Database</title></br>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
+  
 <body>
     <h1>IMDB Movie Database</h1>
-
     <div style="display: flex;">
+
+
     <form action="tabsFinal.php" method="post">
         <input type="submit" name="v_tables" value="View All Tables">
         <input type="submit" name="v_actors" value="View All Actors">
@@ -44,8 +31,54 @@
     <form action="allmoviesFinal.php" method="post">
         <input type="submit" name="v_movies" value="View All Movies">
     </form>
-</div>   
 
+
+
+    </div>
+
+
+    <div class="card" style="border: 3px solid #ccc;">
+    <div class="card-body">
+        <form action="findppl.php" method="post">
+            <div class="form-group">
+                <label for="selectPField">Find Person By Criteria:</label>
+                <select class="form-control" id="selectPField" name="selectPField">
+                    <option value="">Show All People</option>
+                    <!-- <option value="id">ID</option>
+                    <option value="name">Name</option>
+                    <option value="nationality">Nationality</option>
+                    <option value="dob">Date of Birth</option>
+                    <option value="gender">Gender</option> -->
+                </select>
+            </div>
+            <div class="form-group" id="textboxDiv" style="display:none;">
+                <label for="searchTerm">Search Term:</label>
+                <input type="text" class="form-control" id="searchTerm" name="searchTerm" placeholder="Enter search term">
+            </div>
+            <input type="submit" class="btn btn-primary" value="Find People">
+        </form>
+    </div>
+    </div>
+
+    <script>
+        // Show textbox when a field is selected
+        document.getElementById('selectPField').addEventListener('change', function() {
+            var selectField = this.value;
+            var textboxDiv = document.getElementById('textboxDiv');
+            if (selectField !== "") {
+                textboxDiv.style.display = 'block';
+            } else {
+                textboxDiv.style.display = 'none';
+            }
+        });
+    </script>
+
+
+
+
+
+
+   
 
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -119,19 +152,13 @@
                     }
                     $conn->close();
                 }
-            }
-?>  
-
-
+            }   
+        ?>
 </body>
 
 <body>
-    <div class="container">
-        <br>
-        <h1 style="text-align:center">COSI 127 - PA 1.2</h1><br>
-        <h3 style="text-align:center">OUR IMDB</h3><br>
-    </div>
-
+    
+    <br>
     <div class="container">
     <form id="queryForm" method="post" action="php_scriptFinal.php">
         <div class="form-group">
@@ -139,27 +166,32 @@
             <select class="form-control" id="selectTable" name="selectTable">
                 <option value=""></option> <!-- Blank option -->
                 <option value="MotionPicture">MotionPicture</option>
-                <option value="User">User</option>
+                <option value="Users">Users</option>
                 <option value="Likes">Likes</option>
                 <option value="Movie">Movies</option>
                 <option value="Series">Series</option>
                 <option value="People">People</option>
-                <option value="Role">Role</option>
-                <option value="Award">Award</option>
-                <option value="Genre">Genre</option>
-                <option value="Location">Location</option>
+                <option value="Role">Roles</option>
+                <option value="Award">Awards</option>
+                <option value="Genre">Genres</option>
+                <option value="Location">Locations</option
+
+                <!-- Add options for other tables here -->
             </select>
         </div>
         <div class="form-group">
-            <label for="selectField">Select Field:</label>
-            <select class="form-control" id="selectField" name="selectField">
-                <!-- Options will be dynamically populated based on the selected table -->
-            </select>
+        <label for="selectField">Select Field:</label>
+        <select class="form-control" id="selectField" name="selectField">
+            <!-- Initial empty option -->
+            <option value=""></option>
+            <!-- Options will be dynamically populated based on the selected table -->
+        </select>
         </div>
         <div class="form-group">
             <label for="searchTerm">Search Term:</label>
-            <input type="text" class="form-control" id="searchTerm" name="searchTerm" placeholder="Enter search term">
+            <input type="text" class="form-control" id="searchTerm" name="searchTerm" placeholder="Enter search term" value="">
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
@@ -174,13 +206,13 @@
 
         // Populate options array based on selectTable
         if (selectTable === "MotionPicture") {
-            options = ["", "id", "name", "rating", "production", "budget", "movie name", "series name", "shooting location country", "highest rating", "cast"];
-        } else if (selectTable === "User") {
-            options = ["", "email", "name", "age", "likes and ages"];
+            options = ["", "id", "name", "rating", "production", "budget"];
+        } else if (selectTable === "Users") {
+            options = ["", "email", "name", "age"];
         } else if (selectTable === "Likes") {
             options = ["", "uemail", "mpid"];
         } else if (selectTable === "Movie") {
-            options = ["","mpid", "boxoffice_collection"];
+            options = ["", "mpid", "boxoffice_collection"];
         } else if (selectTable === "Series") {
             options = ["", "mpid", "season_count"];
         } else if (selectTable === "People") {
@@ -195,6 +227,7 @@
             options = ["", "mpid", "zip", "city", "country"];
         }
 
+
         // Add options to selectField
         if (options) {
             options.forEach(function(option) {
@@ -206,54 +239,6 @@
         }
     });
 </script>
-
-<script>
-    document.getElementById("queryForm").addEventListener("submit", function(event) {
-        // Retrieve the values of form input fields
-        var selectTable = document.getElementById("selectTable").value;
-        var selectField = document.getElementById("selectField").value;
-        var searchTerm = document.getElementById("searchTerm").value;
-
-        // Check if selectField is "genre_name"
-        if (selectField === "genre_name") {
-            // Change the form action to genre_query.php
-            this.action = "genre_query.php";
-        }
-
-        if (selectField == "likes and ages"){
-            this.action = "likes_users.php"
-        }
-
-        if (selectField == "highest rating"){
-            this.action = "highest.php"
-        }
-
-        if (selectField == "cast"){
-            this.action = "cast_members.php"
-        }
-        // else, the form action remains the same
-
-        // Form submission continues as usual
-    });
-</script>
-
-<script>
-    document.getElementById("selectField").addEventListener("change", function() {
-        var selectField = this.value;
-        var searchTermLabel = document.querySelector('label[for="searchTerm"]');
-
-        // Check if the selected field is "genre_name"
-        if (selectField === "genre_name") {
-            // Change the label text
-            searchTermLabel.textContent = "Enter <genre, location> or <genre>";
-        } else {
-            // Reset the label text to default
-            searchTermLabel.textContent = "Enter search term";
-        }
-    });
-</script>
-
-
 <br>
 <div class="container">
         <h2>Like a Movie</h2>
@@ -269,6 +254,18 @@
             <button type="submit" class="btn btn-primary" name="like">Like</button>
         </form>
     </div>
+
+    <div class="container">
+    <h2>See your likes</h2>
+    <form action="tabsFinal.php" method="post">
+        <div class="form-group">
+            <label for="userEmailLikes">Your Email:</label>
+            <input type="email" class="form-control" id="userEmaiLikes" name="userEmailLikes">
+        </div>
+        <button type="submit" class="btn btn-primary" name="see_likes">See Likes</button>
+    </form>
+    </div>
+
 
     <?php
 // Check if the form has been submitted
